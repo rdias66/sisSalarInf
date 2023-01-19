c88587c8e05a8a3b860dd24f22d902d60c7771d6
rdias66 <88790315+rdias66@users.noreply.github.com>
2023-01-19 08:16:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,28 @@ const menu = () => { //Debuggin version , need trimming
           console.log("3- Comparar e listar a porcentagem de crescimento salarial com a inflação(IPCA)");
           console.log("4- Finalizar aplicação");
           let userInput = rl.question("Dígito informado:");
-          console.log(userInput);
+          console.log("Escolha do usuário: "+ userInput);
           Number(userInput) === 4 ? controller = false : controller = true;
         }while(controller);
 }
+
+const contentPrinter = (content) =>{
+	//prints labels and content	
+
+}
+
+const userChoice1 = (array_db) => { //handles content and calls printer
+	
+}
+
+const userChoice2 = (array_db) => { //handles content and calls printer
+	
+}
+
+
+const userChoice3 = (array_db) => { //handles content and calls printer
+	
+}
+
+
 menu();
